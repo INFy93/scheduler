fa3253d78491172c9d8c5fc338e20d7de3c018a0
INFy93 <infy93@gmail.com>
2023-07-29 22:02:35 +0300

    fix type of MySQL column on goals migration

--- database/migrations/2023_07_13_144139_create_goals_table.php
+++ database/migrations/2023_07_13_144139_create_goals_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('description');
             $table->integer('author_id');
             $table->integer('performer_id');
-            $table->date('goal_time');
+            $table->dateTime('goal_time');
             $table->timestamps();
         });
     }
